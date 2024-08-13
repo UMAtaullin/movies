7158bfc2344746b92679d9b43c78c491a5bc6316
UMAtaullin <ataullinural@gmail.com>
2024-08-13 14:14:29 +0500

    layout with flex

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,7 @@
-import cls from './Footer.module.css'
-
 function Footer() {
   return (
-    <footer className={cls.footer}>
-      © {new Date().getFullYear()} Copyright Text
+    <footer>
+      © {new Date().getFullYear()} Ural Ataullin
     </footer>
   )
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,8 @@
-import cls from './Header.module.css'
-
 function Header() {
   return (
-    <nav className={cls.nav}>
+    <nav>
       <div>
-        <a href='https//google.com' className='brand-logo'>
-          React Movies
-        </a>
-        <ul id='nav-mobile' className='right hide-on-med-and-down'>
-          <li>
-            <a href='https//google.com'>Repo</a>
-          </li>
-        </ul>
+        <a>Ural Movies</a>
       </div>
     </nav>
   )
